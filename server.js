7539a51482aae6e7d63f7d0af21b3e24185845f8
Christina Thompson <c.thompsonms@gmail.com>
2014-06-22 13:58:39 -0700

    created a post method and parsed that request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs   = require("fs");
+var queryString = require("querystring");
 // socket.io
 
 http.createServer(function (request, response) {
@@ -11,6 +12,20 @@ http.createServer(function (request, response) {
   } else if (request.url == "/messages.json") {
     response.writeHead(200, {'Content-Type': 'application/json'});
     response.end("{a: 'b'}");
+  } else if (request.url == "/messages") {
+    var fullBody = '';
+    request.on('data', function(chunk) {
+      fullBody += chunk.toString();
+      var parsedMessage = queryString.parse(fullBody)
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end(parsedMessage.message);
+    });
+    
+    
+    console.log(fullBody)
+  } else {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.end("404");
   }
 }).listen(1337, '127.0.0.1');
 
